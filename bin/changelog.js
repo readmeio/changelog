fb73819ca46d69e5b55f6828ae76984c97fc16fe
Dom Harrington <dom@harrington-mail.com>
2018-03-12 16:04:19 -0700

    Add changelog binary which saves commits to changelog.json file

---
+++ bin/changelog.js
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+const fs = require('fs');
+
+const { join } = require('path');
+const { promisify } = require('util');
+
+const readFile = promisify(fs.readFile);
+const writeFile = promisify(fs.writeFile);
+const gitlog = promisify(require('gitlog'));
+
+const outputFile = join(process.cwd(), 'changelog.json');
+const changelog = require('../');
+
+async function main() {
+  let output;
+  let since;
+  try {
+    output = JSON.parse(await readFile(outputFile));
+    // Have to add a second onto the date otherwise it'll include
+    // the last commit that was made as well
+    since = new Date(output[0].date);
+    since.setSeconds(since.getSeconds() + 1);
+    since = since.toISOString();
+  } catch (e) {
+    output = [];
+    since = null;
+  }
+
+  const commits = await gitlog({ repo: process.cwd(), fields: ['subject', 'authorDate', 'body'], since });
+
+  await writeFile(outputFile, JSON.stringify(changelog(commits).concat(output)));
+}
+
+main().catch((e) => {
+  console.error(e); // eslint-disable-line no-console
+  process.exit(1);
+});
--- test/bin.test.js
+++ test/bin.test.js
@@ -1,6 +1,75 @@
 /* eslint-env mocha */
 
+const cp = require('child_process');
+const os = require('os');
+const moment = require('moment');
+const assert = require('assert');
+const rimraf = require('rimraf');
+const { mkdtempSync } = require('fs');
+const { resolve, join } = require('path');
+
+function makeCommits(cwd, msg = '', date = moment()) {
+  const opts = {
+    cwd,
+    env: {
+      GIT_COMMITTER_DATE: date.toISOString(),
+      GIT_AUTHOR_DATE: date.toISOString(),
+    },
+  };
+  cp.spawnSync('git', ['init'], opts);
+  cp.spawnSync('git', ['commit', '-m', `[feature][new] new feature ${msg}`, '--allow-empty'], opts);
+  cp.spawnSync('git', ['commit', '-m', `non-changelog worthy commit ${msg}`, '--allow-empty'], opts);
+  cp.spawnSync('git', ['commit', '-m', `[feature][fixed] fixed bug feature ${msg}`, '--allow-empty'], opts);
+}
+
+function spawnChangelog(cwd) {
+  const { stderr, stdout } = cp.spawnSync(resolve(__dirname, '..', 'bin', 'changelog.js'), [], { cwd });
+
+  if (stderr.toString()) throw new Error(stderr.toString());
+  if (stdout.toString()) console.log('output', stdout.toString()); // eslint-disable-line no-console
+}
+
 describe('bin', () => {
-  it('should prefix output to changelog.json: newest first');
-  it('should write to changelog.json if it does not exist');
+  let cwd;
+  beforeEach(() => {
+    cwd = mkdtempSync(join(os.tmpdir(), 'changelog-'));
+    makeCommits(cwd, '1', moment().subtract(1, 'day'));
+  });
+
+  afterEach(() => {
+    rimraf.sync(cwd);
+  });
+
+  it('should write to changelog.json if it does not exist', () => {
+    spawnChangelog(cwd);
+
+    // eslint-disable-next-line global-require, import/no-dynamic-require
+    const changelog = require(join(cwd, 'changelog.json'));
+    assert.equal(changelog.length, 2);
+  });
+
+  it('should prefix output to changelog.json: newest first', () => {
+    spawnChangelog(cwd);
+    makeCommits(cwd, '2', moment());
+    spawnChangelog(cwd);
+
+    // eslint-disable-next-line global-require, import/no-dynamic-require
+    const changelog = require(join(cwd, 'changelog.json'));
+
+    assert.equal(changelog.length, 4);
+  });
+
+  it('should only return commits later than the latest', () => {
+    spawnChangelog(cwd);
+
+    // Make some older commits than the initial set, these shouldnt be included
+    makeCommits(cwd, '2', moment().subtract(2, 'day'));
+
+    spawnChangelog(cwd);
+
+    // eslint-disable-next-line global-require, import/no-dynamic-require
+    const changelog = require(join(cwd, 'changelog.json'));
+
+    assert.equal(changelog.length, 2);
+  });
 });
